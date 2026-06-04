9765300e386f1cd44420d13d8c7ca1d0304bbf66
Laith Taher <lotaher04@gmail.com>
2026-06-04 17:00:33 -0400

    update string returns

--- lt_string.c
+++ lt_string.c
@@ -13,15 +13,15 @@ string8 str8_cstring(char *str) { return (string8){(u8 *)str, strlen(str)}; }
 b8 str8_compare(string8 str1, string8 str2)
 {
   if (str1.length != str2.length)
-    return -1;
+    return 0;
 
-  return memcmp(str1.str, str2.str, str1.length) == 0 ? 1 : -1;
+  return memcmp(str1.str, str2.str, str1.length) == 0 ? 1 : 0;
 }
 
 b8 str8_contains(string8 str, string8 substr)
 {
   if (substr.length > str.length)
-    return -1;
+    return 0;
 
   for (u64 i = 0; i < str.length; i++) {
     string8 sub = str8_substring(str, i, substr.length + i);
@@ -30,14 +30,14 @@ b8 str8_contains(string8 str, string8 substr)
       return 1;
   }
 
-  return -1;
+  return 0;
 }
 
 string8 str8_copy(string8 str, arena *arena)
 {
   u8 *bytes = (u8 *)arena_push(arena, str.length);
 
-  memcpy(bytes, &str, str.length);
+  memcpy(bytes, str.str, str.length);
 
   return (string8){bytes, str.length};
 }
